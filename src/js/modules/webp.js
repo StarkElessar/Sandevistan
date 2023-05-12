fb27eb4bd425d2d03024391e619ec3efc25aac51
Сергей <serj.elessar@gmail.com>
2023-05-12 10:42:20 +0300

    Update webp.js

--- src/js/modules/webp.js
+++ src/js/modules/webp.js
@@ -2,11 +2,12 @@ import { html } from '@js/helpers/nodeList'
 
 function webp() {
 	function testWebP(callback) {
-		let webP = new Image()
-		webP.onload = webP.onerror = () => callback(webP.height == 2)
-		webP.src =
-			'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA'
+		const webP = new Image()
+		
+		webP.onload = webP.onerror = () => callback(webP.height === 2)
+		webP.src = 'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA'
 	}
+	
 	testWebP(support => html.classList.add(support === true ? 'webp' : 'no-webp'))
 }
 
